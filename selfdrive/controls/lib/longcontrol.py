937e50f35d92c6ccb6bd504c97672bc47bb7893a
Circuit Pro Industries <oricalworks@gmail.com>
2021-11-13 14:01:35 -0500

    Update longcontrol.py

--- selfdrive/controls/lib/longcontrol.py
+++ selfdrive/controls/lib/longcontrol.py
@@ -23,7 +23,6 @@ def long_control_state_trans(CP, active, long_control_state, v_ego, v_target, v_
                        (v_ego < CP.vEgoStopping and
                         ((v_pid < stopping_target_speed and v_target < stopping_target_speed) or
                          brake_pressed))
-  stopping_condition = stopping_condition or (v_ego < 2.46 and v_target < 3.) #Cleaner method discoverd to make sure we stop on older version of neokii - JPR
   starting_condition = v_target > CP.vEgoStarting and not cruise_standstill
   
   # neokii
